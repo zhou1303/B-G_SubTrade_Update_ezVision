0b0e4ecf89079563502f22ce083e1a431f241125
Sicheng Zhou <38563922+zhou1303@users.noreply.github.com>
2019-10-15 09:47:42 -0500

    Add files via upload

--- Constant.py
+++ Constant.py
@@ -94,8 +94,8 @@ filter_not_begins_with = 'Not Begin With'
 
 # oid_enterprise = 16682284300 #Legacy
 oid_enterprise = 54775198209 #ezVision
-oid_trade_flag_report = 54800873903
-oid_subtrade_flag_report = 54800934130
+oid_trade_flag_report = None
+oid_subtrade_flag_report = None
 
 menu_value_ref_type_trade = '(19217908000,3250,0)'
 menu_value_ref_type_subtrade = '(54144713184,3250,0)'
